faf9ba6dfeba168aa858af8b438ca2f55a918790
HungDoan <doananhlehung@gmail.com>
2025-08-18 21:57:38 +0700

    update source

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -15,14 +15,14 @@ module.exports = {
       env: {
         NODE_ENV: 'production',
         PORT: 3000,
-        NEXT_PUBLIC_API_URL: 'http://24hbilliardscoffee.com/api',
+        NEXT_PUBLIC_API_URL: 'http://api.24hbilliardscoffee.com',
         NEXT_PUBLIC_APP_NAME: 'Phần mềm quản lý 24h Billiard App',
         NEXT_PUBLIC_APP_DESCRIPTION: 'Fullstack application with Laravel backend and Next.js frontend'
       },
       env_production: {
         NODE_ENV: 'production',
         PORT: 3000,
-        NEXT_PUBLIC_API_URL: 'http://24hbilliardscoffee.com/api',
+        NEXT_PUBLIC_API_URL: 'http://api.24hbilliardscoffee.com',
         NEXT_PUBLIC_APP_NAME: 'Phần mềm quản lý 24h Billiard App',
         NEXT_PUBLIC_APP_DESCRIPTION: 'Fullstack application with Laravel backend and Next.js frontend'
       }
